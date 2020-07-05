a8dffb6f722f29d1f841e4585d40f19b9216d1dd
Rodrigo <rodare.cl@gmail.com>
2020-07-05 15:56:53 +0200

    ejemplo

---
+++ audio/main.js
@@ -0,0 +1,25 @@
+var a = document.getElementById("caballo1"); 
+
+function playPause1() { 
+  if (a.paused)
+   a.play();
+  else
+   a.pause(); 
+} 
+
+
+var b = document.getElementById("caballo2"); 
+function playPause2() { 
+  if (b.paused)
+   b.play();
+  else
+   b.pause(); 
+} 
+
+var c = document.getElementById("caballo3"); 
+function playPause3() { 
+  if (c.paused)
+   c.play();
+  else
+   c.pause(); 
+} 
