8d8284a2ee8fe37bad774aeb8113667c4953d4a9
Rodrigo Arenas Catalán <rodare.cl@gmail.com>
2021-09-21 17:14:52 +0200

    sitemap

--- html_form_send.php
+++ html_form_send.php
@@ -96,6 +96,8 @@ $headers = 'From: '.$email_from."\r\n".
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/lightbox.min.css" />
 <script src="js/lightbox-plus-jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/lightbox.min.css" />
+<script src="js/lightbox-plus-jquery.min.js"></script>
 </head>
 
 <body>
